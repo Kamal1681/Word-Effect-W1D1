31b23664133ed53144c652e5ad7f3106ce58aaeb
Kamal Maged <Kamal_Maged@yahoo.com>
2019-01-07 18:32:44 -0500

    Word Effect assignment W1D1

--- Word Effect P1/main.m
+++ Word Effect P1/main.m
@@ -10,8 +10,56 @@
 
 int main(int argc, const char * argv[]) {
     @autoreleasepool {
-        // insert code here...
-        NSLog(@"Hello, World!");
+        // 255 unit long array of characters
+        char inputChars[255];
+        char command[3];
+        char *ptr;//dummy pointer for getting an integer value using strtol function
+        long operation = 5;
+        while (YES)
+        {
+            printf("Input a string: ");
+            // limit input to max 255 characters
+            fgets(inputChars, 255, stdin);
+            
+            // print as a c string
+            printf("Your string is %s\n", inputChars);
+            
+            // convert char array to an NSString object
+            NSMutableString *inputString = [NSMutableString stringWithUTF8String:inputChars];
+
+            NSLog(@"Input was: %@", inputString);
+           
+            printf("Enter a number from 1 to 6 : ");
+            fgets(command, 3, stdin);
+            operation = strtol(command, &ptr, 10);
+            
+            switch (operation) {
+                case 1:
+                    NSLog(@"%@", [inputString uppercaseString]);
+                    break;
+                case 2:
+                    NSLog(@"%@", [inputString lowercaseString]);
+                    break;
+                case 3:
+                    NSLog(@"%d", [inputString intValue]);
+                    break;
+                case 4:
+                    NSLog(@"%@", [[inputString stringByReplacingOccurrencesOfString:@"\n" withString:@""] stringByAppendingString:@",eh?"]);
+                    break;
+                case 5:
+                    if ([inputString hasSuffix:@"?\n"]){
+                        NSLog(@"I don't know");
+                    }
+                    if([inputString hasSuffix:@"!\n"]){
+                        NSLog(@"Whoa, calm down!");
+                    }
+                    break;
+                case 6:
+                    NSLog(@"%@", [inputString stringByReplacingOccurrencesOfString:@" " withString:@"-"]);
+                default:
+                    break;
+            }
+        }
     }
     return 0;
 }
