80311d5102cf9d9e54371604a6c2d81cfdf176d5
Kamal Maged <Kamal_Maged@yahoo.com>
2019-01-07 13:17:44 -0500

    Initial Commit

---
+++ Word Effect P1/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  Word Effect P1
+//
+//  Created by Kamal Maged on 2019-01-07.
+//  Copyright © 2019 Kamal Maged. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
